c540e151f96a7706ec0f100b7140070181ec5f0c
David Lambl <david.lambl@gmail.com>
2023-03-28 23:59:38 -0400

    deck updates

---
+++ test.js
@@ -0,0 +1,19 @@
+function reducer (state, value) {
+  return state + value
+}
+
+function Counter() {
+  const [count, dispatch] = React.useReducer(
+    reducer,
+    0
+  )
+
+  return (
+    <React.Fragment>
+      <h1>{count}</h1>
+      <button onClick={() => dispatch(1)}>
+        +
+      </button>
+    </React.Fragment>
+  );
+}
